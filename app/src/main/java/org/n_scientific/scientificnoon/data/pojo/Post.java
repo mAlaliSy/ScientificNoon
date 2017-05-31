c3bd946f97b5b84897c28f7ef14270a3e067960b
Mohammad AL-Ali <m.alali.sy@gmail.com>
2017-05-31 13:36:13 +0300

    Set Title and Content class to public

--- app/src/main/java/org/n_scientific/scientificnoon/data/pojo/Post.java
+++ app/src/main/java/org/n_scientific/scientificnoon/data/pojo/Post.java
@@ -139,7 +139,7 @@ public class Post implements Serializable {
 
 
     // Represent Title JSON Object
-    class Title implements Serializable{
+    public class Title implements Serializable {
         @SerializedName("rendered")
         @Expose
         private String title;
@@ -154,7 +154,7 @@ public class Post implements Serializable {
     }
 
     // Represent Content JSON Object
-    class Content implements Serializable{
+    public class Content implements Serializable {
         @SerializedName("rendered")
         @Expose
         private String content;
