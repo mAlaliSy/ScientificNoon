c6c9647e7465b702ead29303853a2cf0cd565645
mohsen <talal1mohsen@outlook.com>
2017-06-23 05:05:05 +0300

    Add EditText (Search )

--- app/src/main/java/org/n_scientific/scientificnoon/ui/main/MainActivity.java
+++ app/src/main/java/org/n_scientific/scientificnoon/ui/main/MainActivity.java
@@ -2,16 +2,37 @@ package org.n_scientific.scientificnoon.ui.main;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.EditText;
 
 import org.n_scientific.scientificnoon.MyApplication;
 import org.n_scientific.scientificnoon.R;
+import org.n_scientific.scientificnoon.data.pojo.Post;
+import org.n_scientific.scientificnoon.data.remote.remote_data_sources.CategoriesRemoteDataSource;
 import org.n_scientific.scientificnoon.data.remote.remote_data_sources.PostsRemoteDataSource;
 import org.n_scientific.scientificnoon.ui.BaseActivity;
+import org.n_scientific.scientificnoon.ui.adapters.PostsAdapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 
 import javax.inject.Inject;
+
+import static android.R.attr.onClick;
+import static android.R.attr.visibility;
+import static org.n_scientific.scientificnoon.R.menu.*;
 
 public class MainActivity extends BaseActivity implements MainContract.View {
 
@@ -29,11 +50,13 @@ public class MainActivity extends BaseActivity implements MainContract.View {
     // User to display his posts..
     public static final String USER_KEY = "user";
 
+
     @Inject
     PostsRemoteDataSource postsRemoteDataSource;
 
     @Inject
     MainContract.Presenter presenter;
+
 
 
     @Override
@@ -48,9 +71,11 @@ public class MainActivity extends BaseActivity implements MainContract.View {
         configuration.locale = locale;
         getResources().updateConfiguration(configuration, null);
 
+
     }
 
     @Override
+
     public int getContentResource() {
         return R.layout.activity_main;
     }
@@ -63,5 +88,29 @@ public class MainActivity extends BaseActivity implements MainContract.View {
                         .getRemoteDataSourceComponent())
                 .build()
                 .inject(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.search_view_menu, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView =(SearchView)item.getActionView();
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuSearch:
+                EditText txt = (EditText)findViewById(R.id.txtSearch);
+                txt.setVisibility(View.VISIBLE);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
